16c4d114deaded2f479acbbcd48611f6f193d969
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-10 21:26:30 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,11 @@
 def turn
   puts "Enter your move 1-9:"
+  # receive user input
+  # convert position to an index.
+  # If the move is valid:
+    # make the move
+    # display board to the user
+  # If the move is invalid:
+    # ask for a new move until a valid move is received
+
 end
