086fdd5ea5bb12e73032be33f89ffbbd77e7fdba
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-10 21:34:39 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,6 +13,35 @@
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
+def valid_move?(board, index)
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+       false
+    else
+      true
+    end
+  end
+  
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      true
+    else
+      false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    true
+  else
+    false
+  end
+end
+
+
+
+
+
+
 def update_array_at_with(array, index, value)
   array[index] = value
 end
